4018790b560e65a41c76c1f3d22c0fd4964c0a5f
dwiwahyufauzan <dwiwhy31@gmail.com>
2025-10-20 16:00:13 +0700

    Updates

---
+++ _app/immutable/chunks/CXU7T5sZ.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_slpajc?.base??"/pzn-company-profile",a=globalThis.__sveltekit_slpajc?.assets??s??"";export{a,s as b};
--- _app/immutable/chunks/D87hQoMD.js
+++
@@ -1,1 +0,0 @@
-const s=globalThis.__sveltekit_1jlyb87?.base??"/pzn-company-profile",a=globalThis.__sveltekit_1jlyb87?.assets??s??"";export{a,s as b};
